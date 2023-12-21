08a89fb2b0a57d285ad34d430c690f3e54952f78
duyhai <duyhaipro97@gmail.com>
2023-12-21 15:32:48 +0700

    Test GUI for desktop

---
+++ todolist.py
@@ -0,0 +1,24 @@
+todos = []
+
+
+
+while True:
+    user_action = input("Type add or show: ")
+    user_action.strip(" ")
+
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        
+        case "show":
+            for item in todos:
+                item = item.title()
+                print(item)
+
+        case "exit":
+            break
+
+        case whatever:
+            print("Type again")
