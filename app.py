0253a1f674b3171d1490a8ceaec3aa8984384b36
duyhai <duyhaipro97@gmail.com>
2023-12-21 17:36:27 +0700

    implement ADD button

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import PySimpleGUI as sg
-
+import todolist as td
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key = "todo")
 add_button = sg.Button("Add")
 
 
-window = sg.Window('My to-do app', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('My to-do app', layout=[[label],
+                    [input_box, add_button]],
+                    font=('Helvetica',10))
+
 #close the window
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = td.get_todos()
+            print(todos)
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            print(todos)
+            td.write_todos(todos)
+        case sg.WINDOW_CLOSED:
+            break
 window.close()
--- todolist.py
+++ todolist.py
@@ -1,24 +1,36 @@
-todos = []
+FILEPATH = "data.txt"
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return  todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath,'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("Hello")
 
 
 
-while True:
-    user_action = input("Type add or show: ")
-    user_action.strip(" ")
-
-
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ")
-            todos.append(todo)
-        
-        case "show":
-            for item in todos:
-                item = item.title()
-                print(item)
-
-        case "exit":
-            break
-
-        case whatever:
-            print("Type again")
+# while True:
+#     user_action = input("Type add or show: ")
+#     user_action.strip(" ")
+#
+#
+#     match user_action:
+#         case "add":
+#             todo = input("Enter a todo: ")
+#             todos.append(todo)
+#
+#         case "show":
+#             for item in todos:
+#                 item = item.title()
+#                 print(item)
+#
+#         case "exit":
+#             break
+#
+#         case whatever:
+#             print("Type again")
