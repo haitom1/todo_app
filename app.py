3e4c095f7a4fd4950a12ce6a05a1ae1ca1e45bc6
duyhai <duyhaipro97@gmail.com>
2024-01-03 15:24:07 +0700

    Done app

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import PySimpleGUI as sg
 import todolist as td
 import time
+import os
+# sg.theme('BluePurple')
 
-sg.theme('Dark Amber 5')
+if not os.path.exists("data.txt"):
+    with open("data.txt" , "w") as file:
+        pass
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key = "todo")
-add_button = sg.Button("Add")
+# add_button = sg.Button("Add", image_source="add.png")
+add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="white", tooltip="Add todo", key="Add")
 list_box = sg.Listbox(values=td.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
@@ -18,7 +23,7 @@ window = sg.Window('My to-do app',
 
 #close the window
 while True:
-    event, values = window.read(timeout=10)
+    event, values = window.read(timeout=100)
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     # print(event)
     # print(values)
