648dd6e581c1ab851f39ec6c79bd67dcec2e191f
duyhai <duyhaipro97@gmail.com>
2024-01-08 16:20:01 +0700

    Complete todo app, web

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import  todolist as td
 
 def add_todo():
-    todo = st.session_state["new_todo"]
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     td.write_todos(todos)
 
@@ -15,8 +15,13 @@ st.subheader("this is todo app")
 
 todos = td.get_todos()
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        td.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="todos",placeholder="add new todo...", on_change=add_todo, key='new_todo')
 st.session_state
